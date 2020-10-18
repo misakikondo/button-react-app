51ad31ce1b51319aa6fdbfe415c859db2f665cef
misakikondo <misakikondo718@gmail.com>
2020-10-18 16:48:40 +0900

    ボタン作成
    Bootstrap使用

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {name: 'who are you'};
+  }
+
+  render(){
+    return (
+  <div>
+    <h1>{this.state.name}</h1>
+    <button type="button" className="btn btn-primary" onClick={() => {this.setState({name:'Misaki'})}}>
+     押してね
+    </button>
+    <button type="button" className="btn btn-primary" onClick={() => {this.setState({name:'Tanner'})}}>
+     押してね
+    </button>
+  </div>
+    )
+  }
+
+
 }
 
-export default App;
+export default App
